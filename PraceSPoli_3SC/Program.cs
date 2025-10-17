d9c31ae202a6c716bfbf525b5a533b40a669c04d
zigacek <zigacek@A4-UC>
2025-10-17 11:16:37 +0200

    Prace s cisly

--- PraceSPoli_3SC/Program.cs
+++ PraceSPoli_3SC/Program.cs
@@ -4,16 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("What is your name?");
-            string a = Console.ReadLine();
-            Console.WriteLine(a);
-            Console.WriteLine("OK");
-            Console.WriteLine("NOK");
-            Console.WriteLine("OOOPS");
-            Console.WriteLine("Terry");
+            //zalozit 3 x promennou pro ulozeni cisla
+            //pres consoli nacist hodnoty do promennych
+            //vytvorit funkci co spocita prumer
+            //tu zavolat a zobrazit vysledek do Console
 
+            int cislo1;
+            int cislo2;
+            int cislo3;
+
+            Console.WriteLine("Zadej cislo 1");
+            cislo1 = int.Parse(Console.ReadLine());
+            //to same jako si rozdelit na:
+            //string text = Console.ReadLine();
+            //int.Parse(text);
+
+            Console.WriteLine("Zadej cislo 2");
+            cislo2 = int.Parse(Console.ReadLine());
+
+            if ( int.TryParse(Console.ReadLine(), out cislo3) == false)
+            {
+                Console.WriteLine("Zadana spatna hodnota, nejde o cislo!");
+            }
 
         }
+
+
+
     }
 }
