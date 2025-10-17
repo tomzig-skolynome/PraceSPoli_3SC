f6b6882803a3bd84da323a3287250619824a493f
zigacek <zigacek@A4-UC>
2025-10-17 10:12:08 +0200

    Přidejte soubory projektu.

---
+++ PraceSPoli_3SC/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PraceSPoli_3SC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
